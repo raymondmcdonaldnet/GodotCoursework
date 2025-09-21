686063f6d0d8d3ce90a84be968d8fd96a943cefc
R.L. McDonald <me@rlmcdonald.com>
2025-09-21 12:14:44 -0500

    Add basic main menu

---
+++ 2d-online-multiplayer/main_menu/main_menu.gd
@@ -0,0 +1,11 @@
+class_name MainMenu
+extends Control
+## The game's main menu screen.
+
+
+func _on_host_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.

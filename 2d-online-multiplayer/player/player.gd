a1ca577005ea96aeade6ea904c673f97ec42610b
R.L. McDonald <me@rlmcdonald.com>
2025-09-23 14:23:56 -0500

    Add weapon with mouse aiming

--- 2d-online-multiplayer/player/player.gd
+++ 2d-online-multiplayer/player/player.gd
@@ -5,6 +5,10 @@ extends CharacterBody2D
 ## Store the player's movement input and synchronize it instead of position to 
 ## help prevent cheating.
 var movement_input := Vector2.ZERO
+## The direction the player is aiming at.
+var aim_vector := Vector2.RIGHT
+
+@onready var weapon_root: Node2D = %WeaponRoot
 
 
 func _ready() -> void:
@@ -17,12 +21,15 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	# Should only run on multiplayer authority (this peer). Get movement input.
 	movement_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	aim_vector = weapon_root.global_position.direction_to(weapon_root.get_global_mouse_position())
 
 
 func _physics_process(_delta: float) -> void:
 	# Should only run on the server. Apply movement to self, then everyone else.
 	apply_movement()
 	apply_movement_to_all_peers()
+	apply_aiming()
+	apply_aiming_to_all_peers()
 
 
 ## Instruct all peers to set their velocity and then move.
@@ -37,3 +44,15 @@ func apply_movement() -> void:
 	velocity = movement_input * 100.0
 	move_and_slide()
 	movement_input = Vector2.ZERO
+
+
+## Instruct all peers to aim their gun toward the aim vector.
+func apply_aiming_to_all_peers() -> void:
+	for peer_id: int in multiplayer.get_peers():
+		apply_aiming.rpc_id(peer_id)
+
+
+## Instruct a specific peer to aim their gun toward the aim vector.
+@rpc("any_peer", "call_remote", "unreliable")
+func apply_aiming() -> void:
+	weapon_root.look_at(weapon_root.global_position + aim_vector)

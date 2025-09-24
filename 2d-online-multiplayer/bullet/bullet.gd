7118e358b06210621b26735de86e3035467cbce0
R.L. McDonald <me@rlmcdonald.com>
2025-09-24 10:20:06 -0500

    Implement bullet life time
    Bullets free themselves after a life time timer elapses. I got tripped up by multiplayer synchronization again and learned that the MultiplayerSpawner is responsible for despawning as well as spawning, meaning all spawning and despawning should only happen on the host's machine.
    
    This course is very good.

--- 2d-online-multiplayer/bullet/bullet.gd
+++ 2d-online-multiplayer/bullet/bullet.gd
@@ -4,10 +4,20 @@ extends Node2D
 ## The speed the bullet moves at each frame.
 const SPEED: int = 600
 
+## The bullet's life time in seconds.
+@export var life_time: float = 2.0
+
 ## The scene used to instantiate new bullets.
 static var my_scene: PackedScene = preload("uid://bpbr8yyqb3cfs")
 ## The direction the bullet will move in each frame.
 var movement_direction := Vector2.RIGHT
+
+@onready var life_timer: Timer = %LifeTimer
+
+
+func _ready() -> void:
+	life_timer.wait_time = life_time
+	life_timer.start()
 
 
 func _physics_process(delta: float) -> void:
@@ -21,3 +31,15 @@ static func new_bullet(pos: Vector2, direction: Vector2) -> Bullet:
 	bullet.movement_direction = direction
 	bullet.global_rotation = direction.angle()
 	return bullet
+
+
+## Handle end of life and free self.
+func die() -> void:
+	queue_free()
+
+
+## Respond to life time elapsing.
+func _on_life_timer_timeout() -> void:
+	# Only kill bullets on host so that MultiplayerSpawner can handle despawning.
+	if multiplayer.is_server():
+		die()

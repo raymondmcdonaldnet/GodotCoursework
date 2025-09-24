c3cbdd9a49fa308b0219b7d0b04139dae82ee66b
R.L. McDonald <me@rlmcdonald.com>
2025-09-24 09:39:28 -0500

    Rework bullet synchronization slightly
    This is a bit confusing but even though I went off-course with my architecture, the course helped out a ton here.
    
    The bullets are being spawned on every client by the host's MultiplayerSpawner. That means their _physics_process() runs for every client, which means global_position (which is updated in there) does not need to be synchronized after spawn.
    
    The rotation and movement direction also don't have to be synchronized after spawn. However, the movement direction does need to be synchronized so newly spawned bullets move in the right direction on each client.

--- 2d-online-multiplayer/bullet/bullet.gd
+++ 2d-online-multiplayer/bullet/bullet.gd
@@ -1,9 +1,12 @@
 class_name Bullet
 extends Node2D
 
+## The speed the bullet moves at each frame.
 const SPEED: int = 600
 
+## The scene used to instantiate new bullets.
 static var my_scene: PackedScene = preload("uid://bpbr8yyqb3cfs")
+## The direction the bullet will move in each frame.
 var movement_direction := Vector2.RIGHT
 
 
@@ -16,5 +19,5 @@ static func new_bullet(pos: Vector2, direction: Vector2) -> Bullet:
 	var bullet := my_scene.instantiate() as Bullet
 	bullet.global_position = pos
 	bullet.movement_direction = direction
-	bullet.rotation = direction.angle()
+	bullet.global_rotation = direction.angle()
 	return bullet
